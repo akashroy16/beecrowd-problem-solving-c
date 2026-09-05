146535d44c03f3ab9ac7138f7b0bdf507e737da0
Akash Roy <akashavro4@gmail.com>
2026-09-05 23:55:56 +0600

    Add Sphere.c to calculate volume of a sphere

---
+++ BEGINNER/1011 - Sphere.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double radius, volume;
+    const double pi = 3.14159;
+
+    scanf("%lf", &radius);
+
+    volume = (4.0/3) * pi * (radius * radius * radius);
+
+    printf("VOLUME = %.3lf\n", volume);
+
+    return 0;
+}
