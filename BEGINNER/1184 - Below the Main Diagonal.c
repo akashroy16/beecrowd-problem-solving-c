8ee62d8fe4761e116dd06795e61645913f31cdec
Akash Roy <akashavro4@gmail.com>
2026-09-10 21:12:30 +0600

    Add program to calculate sum below main diagonal

---
+++ BEGINNER/1184 - Below the Main Diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char O;
+    double M[12][12], result = 0.0;
+    
+    scanf("%c", &O);
+    
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &M[i][j]);
+        }
+    }
+
+    for (int i = 1; i < 12; i++) {
+        for (int j = 0; j < i; j++) {
+            result += M[i][j];
+        }
+    }
+
+    if (O == 'M') {
+        result /= 66.0;
+    }
+
+    printf("%.1f\n", result);
+
+    return 0;
+}
