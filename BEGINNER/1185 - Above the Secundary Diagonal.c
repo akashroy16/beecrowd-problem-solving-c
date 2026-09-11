813c843b942df99be9c855b57ad3489f22d1a672
Akash Roy <akashavro4@gmail.com>
2026-09-11 17:41:11 +0600

    Implement matrix sum and average calculation

---
+++ BEGINNER/1185 - Above the Secundary Diagonal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    char operation;
+    double M[12][12];
+    double sum = 0.0;
+    int count = 0;
+
+    scanf("%c", &operation);
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &M[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            if (i + j < 11) {
+                sum += M[i][j];
+                count++;
+            }
+        }
+    }
+
+    if (operation == 'S') {
+        printf("%.1lf\n", sum);
+    } else if (operation == 'M') {
+        if (count > 0) {
+            printf("%.1lf\n", sum / count);
+        }
+    }
+
+    return 0;
+}
