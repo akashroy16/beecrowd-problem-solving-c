5b2702f18094792ea15d3e9686109a536149a7c2
Akash Roy <akashavro4@gmail.com>
2026-09-14 21:42:12 +0600

    Add solution for Odd, Even or Cheating problem

---
+++ BEGINNER/2059 - Odd, Even or Cheating.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int p, j1, j2, r, a;
+    scanf("%d %d %d %d %d", &p, &j1, &j2, &r, &a);
+    int sum = j1 + j2;
+    if(r && !a) printf("Jogador 1 ganha!\n");
+    else if(!r && a) printf("Jogador 1 ganha!\n");
+    else if(r && a) printf("Jogador 2 ganha!\n");
+    else {
+        if(p == 1) {
+            if(sum % 2 == 0) printf("Jogador 1 ganha!\n");
+            else printf("Jogador 2 ganha!\n");
+        } else {
+            if(sum % 2 == 0) printf("Jogador 2 ganha!\n");
+            else printf("Jogador 1 ganha!\n");
+        }
+    }
+    return 0;
+}
